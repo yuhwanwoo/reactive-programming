21ab94279b16b0e73552ded03051b673b20cbe6e
yuhwanwoo <6514687@naver.com>
2025-08-21 09:54:15 +0900

    subscriber example

---
+++ reactive-programming/src/main/java/rxjava/FlowableContinuousRequestSubscriberExample.java
@@ -0,0 +1,22 @@
+package rxjava;
+
+
+import flux.ContinuousRequestSubscriber;
+import io.reactivex.rxjava3.core.Flowable;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class FlowableContinuousRequestSubscriberExample {
+    public static void main(String[] args) {
+        log.info("start main");
+        getItems()
+                .subscribe(new ContinuousRequestSubscriber<>());
+        log.info("end main");
+    }
+
+    private static Flowable<Integer> getItems() {
+        return Flowable.fromIterable(List.of(1, 2, 3, 4, 5));
+    }
+}
