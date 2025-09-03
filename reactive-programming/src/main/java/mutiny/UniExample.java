2629f5811208511ec93ff177a51a9b8d09b5048d
yuhwanwoo <6514687@naver.com>
2025-09-03 16:47:52 +0900

    mutiny exam

---
+++ reactive-programming/src/main/java/mutiny/UniExample.java
@@ -0,0 +1,16 @@
+package mutiny;
+import io.smallrye.mutiny.Uni;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class UniExample {
+    public static void main(String[] args) {
+        getItem()
+                .subscribe()
+                .withSubscriber(new SimpleUniSubscriber<>(Integer.MAX_VALUE));
+    }
+
+    private static Uni<Integer> getItem() {
+        return Uni.createFrom().item(1);
+    }
+}
