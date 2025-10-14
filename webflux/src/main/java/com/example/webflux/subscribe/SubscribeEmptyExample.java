acb4258ffe7e06b3e980a1fd614c132a8a0513d6
yuhwanwoo <6514687@naver.com>
2025-10-14 09:53:59 +0900

    subscribe exam

---
+++ webflux/src/main/java/com/example/webflux/subscribe/SubscribeEmptyExample.java
@@ -0,0 +1,19 @@
+package com.example.webflux.subscribe;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+@Slf4j
+public class SubscribeEmptyExample {
+    public static void main(String[] args) {
+        log.info("start main");
+        Flux.fromIterable(List.of(1, 2, 3, 4, 5))
+                .doOnNext(value -> {
+                    log.info("value: " + value);
+                })
+                .subscribe();
+        log.info("end main");
+    }
+}
