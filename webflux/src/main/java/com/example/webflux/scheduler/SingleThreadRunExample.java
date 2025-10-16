695de458c9ef7b46eadd2bb92234ccd0ef56d6d1
yuhwanwoo <6514687@naver.com>
2025-10-16 11:24:47 +0900

    scheduler exam

---
+++ webflux/src/main/java/com/example/webflux/scheduler/SingleThreadRunExample.java
@@ -0,0 +1,30 @@
+package com.example.webflux.scheduler;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class SingleThreadRunExample {
+    public static void main(String[] args) {
+        log.info("start main");
+        var executor = Executors.newSingleThreadExecutor();
+        try {
+            executor.submit(() -> {
+                Flux.create(sink -> {
+                    for (int i = 1; i <= 5; i++) {
+                        log.info("next: {}", i);
+                        sink.next(i);
+                    }
+                }).subscribeOn(
+                        Schedulers.single()
+                ).subscribe(value -> {
+                    log.info("value: " + value);
+                });
+            });
+        } finally {
+            executor.shutdown();
+        }
+    }
+}
