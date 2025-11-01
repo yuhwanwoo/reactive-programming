eba33605436c8a55f1f0f5538c4d2e24926f52d8
yuhwanwoo <6514687@naver.com>
2025-11-01 11:01:02 +0900

    test 코드 작성

---
+++ webflux/src/test/java/com/example/webflux/reactor/UserServiceReactorTest.java
@@ -0,0 +1,74 @@
+package com.example.webflux.reactor;
+
+
+
+import com.example.webflux.practice.webflux.common.domain.User;
+import com.example.webflux.practice.webflux.reactor.UserReactorService;
+import com.example.webflux.practice.webflux.reactor.repository.ArticleReactorRepository;
+import com.example.webflux.practice.webflux.reactor.repository.FollowReactorRepository;
+import com.example.webflux.practice.webflux.reactor.repository.ImageReactorRepository;
+import com.example.webflux.practice.webflux.reactor.repository.UserReactorRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserServiceReactorTest {
+    UserReactorService userService;
+    UserReactorRepository userRepository;
+    ArticleReactorRepository articleRepository;
+    ImageReactorRepository imageRepository;
+    FollowReactorRepository followRepository;
+
+    @BeforeEach
+    void setUp() {
+        userRepository = new UserReactorRepository();
+        articleRepository = new ArticleReactorRepository();
+        imageRepository = new ImageReactorRepository();
+        followRepository = new FollowReactorRepository();
+
+        userService = new UserReactorService(
+                userRepository, articleRepository, imageRepository, followRepository
+        );
+    }
+
+    @Test
+    void getUserEmptyIfInvalidUserIdIsGiven() throws ExecutionException, InterruptedException {
+        // given
+        String userId = "invalid_user_id";
+
+        // when
+        Optional<User> user = userService.getUserById(userId).blockOptional();
+
+        // then
+        assertTrue(user.isEmpty());
+    }
+
+    @Test
+    void testGetUser() throws ExecutionException, InterruptedException {
+        // given
+        String userId = "1234";
+
+        // when
+        Optional<User> optionalUser = userService.getUserById(userId).blockOptional();
+
+        // then
+        assertFalse(optionalUser.isEmpty());
+        var user = optionalUser.get();
+        assertEquals(user.getName(), "taewoo");
+        assertEquals(user.getAge(), 32);
+
+        assertFalse(user.getProfileImage().isEmpty());
+        var image = user.getProfileImage().get();
+        assertEquals(image.getId(), "image#1000");
+        assertEquals(image.getName(), "profileImage");
+        assertEquals(image.getUrl(), "https://dailyone.com/images/1000");
+
+        assertEquals(2, user.getArticleList().size());
+
+        assertEquals(1000, user.getFollowCount());
+    }
+}
